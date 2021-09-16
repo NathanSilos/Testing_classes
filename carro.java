6f5fff63046ad28bb9cd509c632d79c984c0caab
Nathan de Silos Bernal Papavero <71854613+NathanSilos@users.noreply.github.com>
2021-09-15 21:43:29 -0300

    Update carro.java

--- carro.java
+++ carro.java
@@ -1,49 +1,149 @@
 package Aula;
+public class Carro {
+	
+	/* Construtor */
+	public Carro(String marca, String modelo, String cor, int km, int capacidadeTanque, int kmPorLitro, double preco) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cor = cor;
+		this.capacidadeTanque = capacidadeTanque;
+		this.kmPorLitro = kmPorLitro;
+		this.preco = preco;
+		this.setKm(km);
+		
+		Carro.totalCarros +=1;
+		Carro.precoTotal += this.preco;
+	}
+	
+	//Propriedades/Atributos
+	private String marca;
+	private String modelo;
+	private String cor;
+	private int km;
+	private int capacidadeTanque;
+	private int kmPorLitro;
+	private double preco;
+	private double valorComDesconto;
+	private static int totalCarros;
+	private static double precoTotal;
+	
+	//Métodos
+	int calcularKm() {
+		return this.capacidadeTanque * this.kmPorLitro;
+	}
+	
+	double calcularDesconto(double percentualDescontoVendedor) {
+		
+		if(percentualDescontoVendedor>=0 && percentualDescontoVendedor<=4) {
+			return this.preco * (percentualDescontoVendedor/100);
+		}else if(percentualDescontoVendedor<0) {
+			return 0;
+		}else {
+			return this.preco * 0.04;
+		}
+		
+	}
+	
+	double calcularDesconto(double percentualDescontoVendedor, double percentualDescontoGerente) {
+		
+		//Alterar o percentual do Vendedor se a venda for menor que 0 ou maior que 4
+		if(percentualDescontoVendedor<0) {
+			percentualDescontoVendedor = 0;
+		}else if(percentualDescontoVendedor>4) {
+			percentualDescontoVendedor=4;
+		}
+		
+		//Alterar o percentual do Gerente se a venda for menor que 0 ou maior que 2
+		if(percentualDescontoGerente<0) {
+			percentualDescontoGerente=0;
+		}else if(percentualDescontoGerente>2) {
+			percentualDescontoGerente=2;
+		}
+		
+		double desconto = this.preco * ((percentualDescontoVendedor/100) + percentualDescontoGerente/100);
+		this.valorComDesconto = preco - desconto;
+		
+		return desconto;
+		
+	}
+	
+	//Métodos GETTERS(Pegar) e SETTERS(Inserir)
+	public String getMarca() {
+		return marca;
+	}
 
-public class Main {
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
 
-	public static void main(String[] args) {
-		
-		Carro novoCarro = new Carro();		
-		novoCarro.marca = "VM";
-		novoCarro.modelo = "Fox";
-		novoCarro.cor = "Azul";
-		novoCarro.km = 15560;	
-		
-		System.out.println("Marca: "+ novoCarro.marca);
-		System.out.println("Modelo: "+ novoCarro.modelo);
-		System.out.println("Cor: "+ novoCarro.cor);
-		System.out.println("Km: "+ novoCarro.km);
-		System.out.println("Calculo: "+ novoCarro.calcularKm(50, novoCarro.km));
-		
-		System.out.println();
-		
-		Carro novoCarro2 = new Carro();		
-		novoCarro2.marca = "Chevrolet";
-		novoCarro2.modelo = "Onix";
-		novoCarro2.cor = "Petro";
-		novoCarro2.km = 22350;	
-		
-		System.out.println("Marca: "+ novoCarro2.marca);
-		System.out.println("Modelo: "+ novoCarro2.modelo);
-		System.out.println("Cor: "+ novoCarro2.cor);
-		System.out.println("Km: "+ novoCarro2.km);
-		System.out.println("Calculo: "+ novoCarro2.calcularKm(50, novoCarro2.km));
-		
-		System.out.println();
-		
-		Carro novoCarro3 = new Carro();		
-		novoCarro3.marca = "Fiat";
-		novoCarro3.modelo = "Palio";
-		novoCarro3.cor = "Prata";
-		novoCarro3.km = 0;	
-		
-		System.out.println("Marca: "+ novoCarro3.marca);
-		System.out.println("Modelo: "+ novoCarro3.modelo);
-		System.out.println("Cor: "+ novoCarro3.cor);
-		System.out.println("Km: "+ novoCarro3.km);
-		System.out.println("Calculo: "+ novoCarro3.calcularKm(50, novoCarro3.km));
-		
-		System.out.println();
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getKm() {
+		return km;
+	}
+
+	public void setKm(int km) {
+		if(km >= 0 && km <= 20000) {
+			this.km = km;
+		}
+		else {
+			System.err.println("Km Inválido!!! =(");
+			System.exit(0); // Abandonar o sistema 
+		}
+	}
+
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	public int getKmPorLitro() {
+		return kmPorLitro;
+	}
+
+	public void setKmPorLitro(int kmPorLitro) {
+		this.kmPorLitro = kmPorLitro;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+		Carro.precoTotal += this.preco;
+	}
+
+	public double getValorComDesconto() {
+		return valorComDesconto;
+	}
+
+	public void setValorComDesconto(double valorComDesconto) {
+		this.valorComDesconto = valorComDesconto;
+	}
+
+	public static int getTotalCarros() {
+		return totalCarros;
+	}
+	
+	public static double getPrecoTotal() {
+		return precoTotal;
 	}
 }
